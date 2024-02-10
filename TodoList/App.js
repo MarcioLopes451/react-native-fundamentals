07612cb44b6f38ac0192678ff00d6cdd95640659
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-02-10 10:41:13 +0000

    add:styling

--- TodoList/App.js
+++ TodoList/App.js
@@ -1,49 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { useState } from "react";
 
 export default function App() {
-  const [todo, setTodo] = useState("");
-  const [task, setTask] = useState([]);
-
-  const addTask = () => {
-    if (todo.trim() !== "") {
-      task.push(todo);
-      setTodo("");
-    }
-  };
   return (
     <View style={styles.container}>
-      <Text>Todo List on React-Native</Text>
-      <View>
-        <TextInput value={todo} onChangeText={setTodo} style={styles.input} />
-        <Button title="Add" color="blue" onPress={addTask} />
+      <View style={[styles.box, styles.lightblue]}>
+        <Text>StyleSheet API</Text>
       </View>
-      <View>
-        <Text style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-          {task.map((tasks, id) => (
-            <Text key={id}>
-              <Text>{tasks}</Text>
-            </Text>
-          ))}
-        </Text>
+      <View style={[styles.lightgreen, styles.box]}>
+        <Text>StyleSheet API</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "plum",
-    padding: 60,
+  container: { flex: 1, backgroundColor: "plum", padding: 60 },
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
   },
-  input: {
-    height: 40,
-    borderWidth: 1,
+  lightblue: {
+    backgroundColor: "lightblue",
   },
-  addButton: {
-    backgroundColor: "white",
-    color: "white",
+  lightgreen: {
+    backgroundColor: "lightgreen",
   },
 });
